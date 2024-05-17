f052d1f790c2c1ccc8735c23d10432ca2fa4392f
ksericpro <ksericpro@gmail.com>
2024-05-18 07:41:40 +0800

    prototype camera

--- prototype_camera_2.py
+++ prototype_camera_2.py
@@ -3,11 +3,10 @@ import os
 import config
 import threading
 
-BATCH = 10
-
 class Camera(object):
     def __init__(self, cam_id):
-        self.frames = []
+        self.frames_1 = []
+        self.frames_2 =[]
         print("Camera Class = {}".format(cam_id))
         self.cam_id = cam_id
         self.img_url = "{}/{}/".format(config.img_url, cam_id)
@@ -41,20 +40,25 @@ class Camera(object):
         try:
             if self.cam_id != -1:
                 self.startThreads()
-                arr = self.getfiles(dir, BATCH)
+                arr = self.getfiles(dir, config.BATCH)
+                print("There are {} file(s)".format(len(arr)))
                 for f in arr:
                     f2 = os.path.join(dir,f)
                     print("adding {}".format(f2))
-                    self.frames.append(open(f2, 'rb').read())
+                    self.frames_1.append(open(f2, 'rb').read())
                 #self.frames = [open(f + '.jpg', 'rb').read() for f in [dir + '1', self.img_url + '2', dir + '3']]
-                print("self.frames size={}".format(len(self.frames)))
+                print("self.frames size={}".format(len(self.frames_1)))
                 self.deletefiles(dir, arr)
         except Exception as e:
             print(e)
         
     def get_frame(self):
-        if (self.frames is not None):
+        if (self.frames_1 is not None):
             #print("Getting frame for {}".format(self.cam_id))
-            return self.frames[int(time()) % len(self.frames)]
+            n = int(time()) % len(self.frames_1)
+            if n == len(self.frames_1) -1:
+                print("End of Frame!!")
+            print("{}:Getting frame for {}, n={}".format(time(), self.cam_id, n))
+            return self.frames_1[n]
         else:
             return None
